c1a584cc89651534aa396ddbd205e2cacaeba638
tom_glb <goodMorning_glb@atguigu.com>
2020-01-03 20:42:04 +0800

    提交测试工作

---
+++ src/com/haoke/bean/Student.java
@@ -0,0 +1,25 @@
+package com.haoke.bean;
+
+public class Student {
+
+	private Integer id;
+	
+	private String username;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
